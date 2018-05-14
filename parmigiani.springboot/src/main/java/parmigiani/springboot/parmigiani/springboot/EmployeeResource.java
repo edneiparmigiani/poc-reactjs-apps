139aba0ccfe9d64ba0a8137c07c98d35f5f04468
Ednei José Parmigiani Júnior <ednei.parmigiani@gmail.com>
2018-05-14 10:28:07 -0300

    Novas implementações

--- parmigiani.springboot/src/main/java/parmigiani/springboot/parmigiani/springboot/EmployeeResource.java
+++ parmigiani.springboot/src/main/java/parmigiani/springboot/parmigiani/springboot/EmployeeResource.java
@@ -22,7 +22,7 @@ public class EmployeeResource {
 
     @RequestMapping(value = "/employee/{id}", method = RequestMethod.GET)
     public ResponseEntity<Employee> get(@PathVariable("id") long id) {
-        Employee e1 = new Employee(1l,"Ednei José", "Parmigiani Júnior", 30);
+        Employee e1 = new Employee(1l, "Ednei José", "Parmigiani Júnior", 30);
         e1.add(linkTo(methodOn(EmployeeResource.class).get(1)).withSelfRel());
 
         return new ResponseEntity<Employee>((e1), HttpStatus.OK);
@@ -32,23 +32,23 @@ public class EmployeeResource {
     public ResponseEntity<List<Employee>> list() {
         List<Employee> employees = new ArrayList<Employee>();
 
-        Employee e1 = new Employee(1l,"Ednei José", "Parmigiani Júnior", 30);
+        Employee e1 = new Employee(1l, "Ednei José", "Parmigiani Júnior", 30);
         e1.add(linkTo(methodOn(EmployeeResource.class).get(1)).withSelfRel());
         employees.add(e1);
 
-        Employee e2 = new Employee(2l,"Bruna Teixeira", "de França", 28);
+        Employee e2 = new Employee(2l, "Bruna Teixeira", "de França", 28);
         e2.add(linkTo(methodOn(EmployeeResource.class).get(2)).withSelfRel());
         employees.add(e2);
 
-        Employee e3 = new Employee(3l,"Bruna Caroline", "Lourenzone Parmigiani", 24);
+        Employee e3 = new Employee(3l, "Bruna Caroline", "Lourenzone Parmigiani", 24);
         e3.add(linkTo(methodOn(EmployeeResource.class).get(3)).withSelfRel());
         employees.add(e3);
 
-        Employee e4 = new Employee(4l,"Adriana", "Lourenzone Parmigiani", 50);
+        Employee e4 = new Employee(4l, "Adriana", "Lourenzone Parmigiani", 50);
         e4.add(linkTo(methodOn(EmployeeResource.class).get(4)).withSelfRel());
         employees.add(e4);
 
-        Employee e5 = new Employee(5l,"Huco Teixeira", "de França", 24);
+        Employee e5 = new Employee(5l, "Huco Teixeira", "de França", 24);
         e5.add(linkTo(methodOn(EmployeeResource.class).get(5)).withSelfRel());
         employees.add(e5);
 
